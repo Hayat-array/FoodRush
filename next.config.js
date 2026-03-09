3a7b403be628a042c99ac3b982ffd53874ff2c39
Hayat Ali <159122933+Hayat-array@users.noreply.github.com>
2026-03-09 18:20:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['mongoose'],
   reactStrictMode: true,
   images: {
     remotePatterns: [
